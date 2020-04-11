db900f41008d20e76a32671b96064ba2bdf21b4e
Luana da Silva <eca.luds@gmail.com>
2020-04-11 11:27:52 -0300

    [fix] Change saving file method

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import pandas as pd
 import csv
 from os import system, name 
 from subprocess import call 
-    
+	
 def clear(): 
-    _ = call('clear' if name =='posix' else 'cls') 
+	_ = call('clear' if name =='posix' else 'cls') 
 
-def separate_files():
-	df_criteria = pd.read_csv('criterios_inclusao_exclusao.csv')
+def save_file(df_criteria):
+	print('\n\nO arquivo esta sendo salvo...\n')
+	df_criteria.to_csv('criterios_inclusao_exclusao.csv', index=False)
+
+def separate_files(df_criteria):
+
 	incluir = df_criteria[~df_criteria['Exclusion'].notna()]
 	incluir.to_csv('artigos_incluidos.csv')
+	
 	excluir = df_criteria[df_criteria['Exclusion'].notna()]
 	excluir.to_csv('artigos_excluidos.csv')
 
@@ -35,14 +40,8 @@ def main():
 
 	try:
 		for ix, row in df.iterrows():
-			if ix+1 == len(df):
-				resp = input('Voce deseja separar os arquivos a incluir e a excluir? (\'s\' ou \'n\')')
-				if resp == 's':
-					separate_files()
-				else:
-					print('Bom trabalho! Ate mais..')
 
-			elif ix+1 not in done_ids:
+			if ix+1 not in done_ids:
 				clear()
 				print(f'Artigo {ix+1} de {len(df)}')
 				print('\n--- Title ---')
@@ -56,12 +55,12 @@ def main():
 				data = {
 					'ID': ix+1,
 					'Author': row['Author'],
-				    'Title': row['Title'],
-				    'DOI': row['DOI'],
-				    'URL': row['Url'],
-				    'Publication Year': row['Publication Year'],
-				    'Inclusion': [inclusion],
-				    'Exclusion': [exclusion]
+					'Title': row['Title'],
+					'DOI': row['DOI'],
+					'URL': row['Url'],
+					'Publication Year': row['Publication Year'],
+					'Inclusion': [inclusion],
+					'Exclusion': [exclusion]
 				}
 				
 				if df_criteria.empty:
@@ -72,10 +71,11 @@ def main():
 
 				input('\nClique para continuar.. (CTRL+C para finalizar)')
 
-
+		else:
+			save_criteria(df_criteria)
+				
 	except:
-		print('\n\nO arquivo esta sendo salvo...\n')
-		df_criteria.to_csv('criterios_inclusao_exclusao.csv', index=False)
+		save_criteria(df_criteria)
 
 if __name__ == '__main__':
 	main()
